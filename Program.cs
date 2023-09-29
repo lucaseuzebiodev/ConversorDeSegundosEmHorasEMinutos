9e0ef53fed3463db8dcd2d4a0d378b4bff7cf1fa
Lucas Euzébio <100422229+lucaseuzebiodev@users.noreply.github.com>
2023-09-29 11:46:05 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nelio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int N, horas, resto, minutos, segundos;
+
+            Console.WriteLine("Digite o número de segundos:");
+
+            N = int.Parse(Console.ReadLine());
+
+            horas = N / 3600;
+            resto = N % 3600;
+
+            minutos = resto / 60;
+            segundos = resto % 60;
+
+            Console.WriteLine(horas + ":" + minutos + ":" + segundos);
+
+
+            Console.ReadLine();
+        }
+    }
+}
